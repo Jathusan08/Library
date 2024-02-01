a4fb886bf64aa9b92a1d1dd6e78a5c866ffa28ec
Jathusan Uthayakumaran <juthayakumara21@hotmail.co.uk>
2024-02-01 16:40:39 +0000

    Add validations on the add book form

--- script.js
+++ script.js
@@ -4,6 +4,105 @@ const addBookBtn = document.querySelector(".add-book-btn");
 const closeButton = document.querySelector(".close-button");
 const submitButton = document.querySelector(".submit-button");
 
+// user input fields
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const radioButtons = document.querySelectorAll('input[name="readingStatus"]');
+const customValueInput = document.getElementById("customValue");
+
+// Error labels
+const titleError = document.querySelector(".title > .error");
+const authorError = document.querySelector(".author > .error");
+const pageNumError = document.querySelector(".pages > .error");
+const userInputPageNumberError = document.querySelector(
+  ".pageNumberInput > .error"
+);
+
 addBookBtn.addEventListener("click", () => {
   modal.showModal();
+});
+
+const showorHideCustomRadioButton = (boolean1, boolean2) => {
+  document.getElementById("customRadio").disabled = boolean1;
+  customValueInput.disabled = boolean2;
+};
+
+showorHideCustomRadioButton(true);
+
+titleInput.addEventListener("input", () => {
+  if (titleInput.value === "" || titleInput.value.trim() === "") {
+    titleError.textContent = "Book title required";
+    titleError.style.color = "red";
+  } else {
+    titleError.textContent = "✓";
+    titleError.style.color = "green";
+  }
+});
+
+authorInput.addEventListener("input", () => {
+  if (authorInput.value === "" || authorInput.value.trim() === "") {
+    authorError.textContent = "Author name required";
+    authorError.style.color = "red";
+  } else {
+    authorError.textContent = "✓";
+    authorError.style.color = "green";
+  }
+});
+
+pagesInput.addEventListener("input", (event) => {
+  if (pagesInput.value === "") {
+    pageNumError.textContent = "Page number required";
+    pageNumError.style.color = "red";
+    showorHideCustomRadioButton(true, true);
+  } else if (pagesInput.value === "0") {
+    pageNumError.textContent = "Page number cannot be equal to 0";
+    pageNumError.style.color = "red";
+    showorHideCustomRadioButton(true, true);
+  } else {
+    pageNumError.textContent = "✓";
+    pageNumError.style.color = "green";
+    showorHideCustomRadioButton(false, true);
+  }
+});
+
+radioButtons.forEach(function (radioButton) {
+  radioButton.addEventListener("change", function () {
+    // Check if the radio button is checked
+    if (radioButton.checked) {
+      if (
+        radioButton.value === "reading" ||
+        radioButton.value === "unread" ||
+        radioButton.value === "completed"
+      ) {
+        customValueInput.value = "";
+        customValueInput.disabled = true;
+        userInputPageNumberError.textContent = "";
+      } else if (radioButton.value === "customValue") {
+        showorHideCustomRadioButton(false, false);
+      }
+    }
+  });
+});
+
+customValueInput.addEventListener("input", () => {
+  if (customValueInput.value === "") {
+    userInputPageNumberError.textContent = "Number of pages read required";
+    userInputPageNumberError.style.color = "red";
+  } else if (
+    customValueInput.value === "0" ||
+    Number(customValueInput.value) < 0
+  ) {
+    userInputPageNumberError.textContent =
+      "Number of pages read cannot be equal to 0 or less than 0";
+    userInputPageNumberError.style.color = "red";
+  } else if (Number(customValueInput.value) > Number(pagesInput.value)) {
+    userInputPageNumberError.textContent =
+      "Number of pages read cannot be greater than total pages of the book";
+    userInputPageNumberError.style.fontSize = "0.5rem";
+    userInputPageNumberError.style.color = "red";
+  } else if (Number(customValueInput.value) <= Number(pagesInput.value)) {
+    userInputPageNumberError.textContent = "✓";
+    userInputPageNumberError.style.color = "green";
+  }
 });
